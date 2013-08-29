2180d1e8afcc9057e3448f904a0dfe479c24d857
Julien Eluard <julien.eluard@gmail.com>
2013-08-29 20:23:08 -0300

    First pass at examples.

---
+++ examples/src/examples/range_with_threshold.cljs
@@ -0,0 +1,44 @@
+(ns examples.range-with-threshold
+  (:require [lucuma.event :refer [fire]])
+  (:require-macros [lucuma :refer [defwebcomponent]]))
+
+(def ^:private previous-value (atom nil))
+
+(defn- add-threshold-crossed-class
+  [el]
+  (.add (.-classList el) "threshold-crossed"))
+
+(defn- remove-threshold-crossed-class
+  [el]
+  (.remove (.-classList el) "threshold-crossed"))
+
+(defn- breach-threshold
+  [el v t]
+  (add-threshold-crossed-class el)
+  (fire el "threshold-crossed" {:type :breached :value v :threshold t}))
+
+(defn- clear-threshold
+  [el v]
+  (remove-threshold-crossed-class el)
+  (fire el "threshold-cleared" {:type :cleared :value v}))
+
+(defn- fire-event-on-threshold-cross
+  [el value min-threshold max-threshold]
+  (cond
+    (> min-threshold value) (breach-threshold el value min-threshold)
+    (> value max-threshold) (breach-threshold el value max-threshold)
+    :default (clear-threshold el value)))
+
+(defn initialize
+  [el min-threshold max-threshold]
+  (.addEventListener el "change" #(fire-event-on-threshold-cross (aget % "target") (aget % "target" "value") min-threshold max-threshold) false))
+
+;;Extending native element currently fails with polymer polyfill
+;;TODO https://github.com/Polymer/ShadowDOM/issues/190
+
+(defwebcomponent lucu-range-with-threshold
+  ;;[min-threshold max-threshold]
+  :extends "input"
+  :style "input[type='range'] .threshold-crossed { background-color: red; }"
+  :entered-document-fn #(initialize % (or min-threshold 10) (or max-threshold 30))
+  :event-handlers #{:threshold-crossed})
