f9636e32e9b68318dc065d89ada4459b11a782c8
Julien Eluard <julien.eluard@gmail.com>
2013-09-12 09:00:20 -0300

    Added cljsbuild conf.

--- project.clj
+++ project.clj
@@ -3,5 +3,16 @@
   :url "http://github.com/jeluard/lucuma"
   :license  {:name "Eclipse Public License"
              :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :min-lein-version "2.3.0")
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1878"]
+                 [com.cemerick/clojurescript.test "0.0.4" :scope "test"]
+                 [prismatic/dommy "0.1.1" :scope "test"]]
+  :plugins [[lein-cljsbuild "0.3.2"]]
+  :hooks [leiningen.cljsbuild]
+  :cljsbuild {:builds [{:source-paths ["src" "test" "examples"]
+                        :compiler {:output-to "target/cljs/lucuma+tests.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]
+              :test-commands {"unit-tests" ["runners/phantomjs.js" "target/cljs/lucuma+tests.js"]}}
+  :min-lein-version "2.3.0"
+  :repositories  {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
