8712dcbb290d5b71a399d653ecee2542751260f5
Julien Eluard <julien.eluard@gmail.com>
2013-09-18 19:26:49 -0300

    Added some comment.

--- resources/extern.js
+++ resources/extern.js
@@ -1,3 +1,4 @@
+//Default extern files as defined by google closure can be found here: https://code.google.com/p/closure-compiler/source/browse/externs/
 Element.prototype.createShadowRoot = function() {};
 DOMImplementation.prototype.register = function(name, options) {};
 
