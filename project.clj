5a999801c7393ccb8152d4b65b93ab6a5a39a9e2
Julien Eluard <julien.eluard@gmail.com>
2013-10-03 18:32:23 -0300

    Moved to latest ClojureScript build.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license  {:name "Eclipse Public License"
              :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1896"]
+                 [org.clojure/clojurescript "0.0-1909"]
                  [com.cemerick/clojurescript.test "0.0.4" :scope "test"]
                  [prismatic/dommy "0.1.2-SNAPSHOT" :scope "test"]
                  [core.async "0.1.0-SNAPSHOT"]
