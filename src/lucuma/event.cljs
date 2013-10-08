3c7dd17d7f9c09dd0140633ac01b31b897c677cf
Julien Eluard <julien.eluard@gmail.com>
2013-10-08 16:20:07 -0300

    Fixed event generation.

--- src/lucuma/event.cljs
+++ src/lucuma/event.cljs
@@ -1,11 +1,12 @@
 (ns lucuma.event)
 
 (defn create-event
-  [n m can-bubble cancelable]
-  (let [ev (.createEvent js/document "CustomEvent")]
-    (.initCustomEvent ev n can-bubble cancelable (clj->js m))))
+  [n m bubbles cancelable]
+  (if (and (empty? m) (not bubbles) (not cancelable))
+    (js/CustomEvent. (name n))
+    (js/CustomEvent. (name n) (clj->js {:bubbles bubbles :cancelable cancelable :detail m}))))
 
 (defn fire
   ([el n] (fire el n {}))
   ([el n m] (fire el n m false false))
-  ([el n m can-bubble cancelable] (.dispatchEvent el (create-event n m can-bubble cancelable))))
+  ([el n m bubbles cancelable] (.dispatchEvent el (create-event n m bubbles cancelable))))
