2ce0ff23f910a6f0468cb197daaa4b36965ddc68
Julien Eluard <julien.eluard@gmail.com>
2013-10-11 11:12:44 -0300

    Improved input-range example.

--- examples/src/lucuma/range_with_threshold.cljs
+++ examples/src/lucuma/range_with_threshold.cljs
@@ -11,29 +11,30 @@
 (defn- breach-threshold
   [el v t]
   (add-threshold-crossed-class el)
-  (fire el :threshold-crossed {:type :breached :value v :threshold t}))
+  (fire el :threshold-cross {:type :breached :value v :threshold t}))
 
 (defn- clear-threshold
   [el v]
   (remove-threshold-crossed-class el)
-  (fire el :threshold-cleared {:type :cleared :value v}))
+  (fire el :threshold-cross {:type :cleared :value v}))
 
 (defn- fire-event-on-threshold-cross
-  [el value min-threshold max-threshold]
-  (cond
-    (> min-threshold value) (breach-threshold el value min-threshold)
-    (> value max-threshold) (breach-threshold el value max-threshold)
-    :default (clear-threshold el value)))
+  [el min-threshold max-threshold]
+  (let [value (or (aget el "value") (aget el "impl" "value"))] ;; TODO get polymer GeneratedWrapper. Bug?
+    (cond
+      (> min-threshold value) (breach-threshold el value min-threshold)
+      (> value max-threshold) (breach-threshold el value max-threshold)
+      :default (clear-threshold el value))))
 
 (defn initialize
   [el min-threshold max-threshold]
-  (.addEventListener el "change" #(fire-event-on-threshold-cross (aget % "target") (aget % "target" "value") min-threshold max-threshold) false))
+  (.addEventListener el "change" #(fire-event-on-threshold-cross (aget % "target") min-threshold max-threshold) false))
 
 (defwebcomponent lucu-range-with-threshold
   :extends "input"
   :style "input[type='range'] .threshold-crossed { background-color: red; }"
   :created-fn #(initialize % (or (get-attribute % "min-threshold") 10) (or (get-attribute % "max-threshold") 30))
-  :handlers #{:threshold-crossed})
+  :handlers #{:threshold-cross})
 
 ;; Chrome doesn't support dynamically created shadow root on input element so the style is not applied.
 ;; https://code.google.com/p/chromium/issues/detail?id=263940
