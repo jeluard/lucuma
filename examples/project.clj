6b55593391a373fc82f61124b73457fb8913af65
Julien Eluard <julien.eluard@gmail.com>
2013-10-30 23:09:49 -0300

    Added tests for range-with-threshold example.

--- examples/project.clj
+++ examples/project.clj
@@ -3,18 +3,20 @@
                  [lucuma "0.2.0-SNAPSHOT"]
                  [prismatic/dommy "0.1.2"]
                  [garden "1.1.3"]]
-  :plugins [[lein-cljsbuild "0.3.4"]]
-  :cljsbuild {:builds [{:source-paths ["src"]
-                        :compiler {:output-to "target/cljs/lucuma+examples.js"
-                                   :source-map "target/cljs/lucuma+examples.js.map"
-                                   :optimizations :simple
-                                   :pretty-print true}}
-                       {:id "production"
-                        :source-paths ["src"]
+  :plugins [[lein-cljsbuild "0.3.4"]
+            [com.cemerick/clojurescript.test "0.1.0"]]
+  :cljsbuild {:builds [{:source-paths ["src" "test"]
                         :compiler {:output-to "target/cljs/lucuma+examples.js"
                                    :optimizations :advanced
-                                   :pretty-print false}}]}
-  :aliases {"clean-build" ["do" "clean," "cljsbuild" "clean," "cljsbuild" "once" "production"]
-            "clean-dev"  ["do" "clean," "cljsbuild" "clean," "cljsbuild" "auto"]}
+                                   :pretty-print false}}
+                       {:id "dev"
+                        :source-paths ["src" "test"]
+                        :compiler {:output-to "target/cljs/lucuma+examples.js"
+                                   :source-map true
+                                   :optimizations :none}}]
+              :test-commands {"slimerjs" ["slimerjs" :runner "target/cljs/lucuma+examples.js"]}}
+  :aliases {"clean-test" ["do" "clean," "cljsbuild" "clean," "cljsbuild" "test"]
+            "clean-build" ["do" "clean," "cljsbuild" "clean," "cljsbuild" "once"]
+            "clean-dev"  ["do" "clean," "cljsbuild" "clean," "cljsbuild" "auto" "dev"]}
   :min-lein-version "2.3.0"
   :repositories  {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
