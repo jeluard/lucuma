3dd3c88f4319973c8989e68fa3c2005e82fcbe56
Julien Eluard <julien.eluard@gmail.com>
2013-11-03 16:56:09 -0300

    Added notifier hook.

--- examples/project.clj
+++ examples/project.clj
@@ -3,20 +3,30 @@
                  [lucuma "0.2.0-SNAPSHOT"]
                  [prismatic/dommy "0.1.2"]
                  [garden "1.1.3"]]
-  :plugins [[lein-cljsbuild "0.3.4"]
+  :plugins [[lein-cljsbuild "1.0.0-SNAPSHOT"]
             [com.cemerick/clojurescript.test "0.1.0"]]
-  :cljsbuild {:builds [{:source-paths ["src" "test"]
+  :cljsbuild {:builds [{:id "prod"
+                        :source-paths ["src" "test"]
                         :compiler {:output-to "target/cljs/lucuma+examples.js"
                                    :optimizations :advanced
                                    :pretty-print false}}
                        {:id "dev"
                         :source-paths ["src" "test"]
-                        :compiler {:output-to "target/cljs/lucuma+examples.js"
-                                   :source-map true
-                                   :optimizations :none}}]
+                        :compiler {:output-to "../../lucuma-doc/assets/lucuma+examples.js"
+                                   :optimizations :simple}
+                        :notify-command ["terminal-notifier"
+                                         "-title"
+                                         "Lucuma"
+                                         "-subtitle"
+                                         "dev"
+                                         "-group"
+                                         "lucuma-build"
+                                         "-activate"
+                                         "com.googlecode.iTerm2"
+                                         "-message"]}]
               :test-commands {"slimerjs" ["slimerjs" :runner "target/cljs/lucuma+examples.js"]}}
   :aliases {"clean-test" ["do" "clean," "cljsbuild" "clean," "cljsbuild" "test"]
-            "clean-build" ["do" "clean," "cljsbuild" "clean," "cljsbuild" "once"]
+            "clean-build" ["do" "clean," "cljsbuild" "clean," "cljsbuild" "once" "prod"]
             "clean-dev"  ["do" "clean," "cljsbuild" "clean," "cljsbuild" "auto" "dev"]}
   :min-lein-version "2.3.0"
   :repositories  {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
