a61351b3a5f3fdd0bb7f25860e14ad43d18f56b9
Julien Eluard <julien.eluard@gmail.com>
2013-11-29 00:44:16 +0100

    Improved polyfill installation detection.

--- src/lucuma/polymer.cljs
+++ src/lucuma/polymer.cljs
@@ -3,13 +3,18 @@
 (defn ^:export shadow-dom-installed?
   "Returns true if Shadow DOM polyfill is installed in current platform."
   []
-  (= "polymer" js/Platform.flags.shadow))
+  (= "polyfill" js/Platform.flags.shadow))
+
+(defn ^:export shadow-css-installed?
+  "Returns true if Shadow CSS polyfill is installed in current platform."
+  []
+  (exists? js/Platform.ShadowCSS))
 
 (defn shim-styling-when-needed
   "Make sure styles do not leak when using polymer polyfill.
   See https://github.com/Polymer/ShadowDOM/issues/260."
   [sr n base-type]
-  (when js/Platform.ShadowCSS
+  (when (shadow-css-installed?)
     (if base-type
       (.shimStyling js/Platform.ShadowCSS sr n (name base-type))
       (.shimStyling js/Platform.ShadowCSS sr n))))
