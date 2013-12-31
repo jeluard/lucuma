39644a4d303d27b77e05e0b63b8d4c43ce68342e
Julien Eluard <julien.eluard@gmail.com>
2013-12-31 09:21:02 -0300

    Cleanup.

--- src/lucuma/attribute.cljs
+++ src/lucuma/attribute.cljs
@@ -1,25 +1,17 @@
-(ns lucuma.attribute
-  (:require [lucuma.util :as u])
+(ns lucuma.attribute)
   ;;(:require-macros [cljs.core.typed :refer [ann]])
-  )
 
 ;;(ann get-attr [js/HTMLElement (U String Keyword) -> (U nil String)])
 (defn get-attr
   [el n]
-  (if (.hasAttribute el (name n))
-    (.getAttribute el (name n))
-    nil))
+  (let [n (name n)]
+    (if (.hasAttribute el n)
+      (.getAttribute el n)
+      nil)))
 
 (defn set-attr!
   [el n v]
-  (if v
-    (.setAttribute el (name n) v)
-    (.removeAttribute el (name n))))
-
-(defn- as-property
-  [n]
-  {:configurable true :enumerable true :get (u/wrap-with-callback-this-value #(get-attr % n)) :set (u/wrap-with-callback-this-value #(set-attr! %1 n %2))})
-
-(defn properties
-  [attributes]
-  (apply merge (map #(hash-map (keyword %) (as-property %)) attributes)))
+  (let [n (name n)]
+    (if v
+      (.setAttribute el n v)
+      (.removeAttribute el n))))
