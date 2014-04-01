7d2d660e423381b9a0e7ada0f103899b1bd4be87
Julien Eluard <julien.eluard@gmail.com>
2014-04-01 17:19:56 -0300

    Upgraded Clojures, ClojureScript and Polymer.

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
              :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths  ["src" "test"]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2197"]]
+                 [org.clojure/clojurescript "0.0-2199"]]
   :plugins [[lein-bower "0.2.0"]
-            [lein-cljsbuild "1.0.2"]
+            [lein-cljsbuild "1.0.3"]
             [com.cemerick/clojurescript.test "0.2.2"]]
   :cljsbuild {:builds [{:source-paths ["src" "test"]
                         :compiler {:output-to "target/cljs/lucuma.js"
@@ -17,7 +17,7 @@
                               ;; TODO Add support for http://triflejs.org/
                               ;; SlimerJS doesn't support exit code so CI won't fail when tests fail. See https://github.com/laurentj/slimerjs/issues/50.
                               "slimerjs" ["slimerjs" :runner "bower_components/polymer-platform/platform.js" "target/cljs/lucuma.js"]}}
-  :bower-dependencies [[polymer-platform "0.2.1"]]
+  :bower-dependencies [[polymer-platform "0.2.2"]]
   :aliases {"clean-test" ["do" "clean," "bower" "install," "cljsbuild" "clean," "cljsbuild" "test"]
             "clean-install" ["do" "clean," "cljsbuild" "clean," "install"]}
   :min-lein-version "2.3.4")
