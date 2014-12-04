49057ea94af873cb618ee28807158d6ac83e810b
Julien Eluard <julien.eluard@gmail.com>
2014-12-04 14:09:57 -0300

    Added browser based test runner.

---
+++ examples/project.clj
@@ -0,0 +1,21 @@
+(defproject lucuma-examples "0.2.1-SNAPSHOT"
+            ;:source-paths ["src" "test"]
+  :profiles
+  {:dev
+   {:dependencies [[org.clojure/clojure "1.6.0"]
+                   [org.clojure/clojurescript "0.0-2371"]
+                   [lucuma "0.2.1-SNAPSHOT"]
+                   [prismatic/dommy "0.1.3"]
+                   [hipo "0.1.0"]]
+    :plugins [[lein-cljsbuild "1.0.3"]
+              [com.cemerick/clojurescript.test "0.3.1"]]}}
+  :cljsbuild
+  {:builds
+   {:test
+    {:source-paths ["src"]
+     :compiler {:output-to "target/cljs/unit-test.js"
+                :optimizations :whitespace}}}}
+  :aliases {"clean-install" ["do" "clean," "cljsbuild" "clean," "install"]}
+  :min-lein-version "2.5.0")
+
+
