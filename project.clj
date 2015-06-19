9349e8ec16e83d603b3f3dadb94ce6e96c0d35db
Julien Eluard <julien.eluard@gmail.com>
2015-06-18 23:53:54 -0300

    Upgraded dependencies.

--- project.clj
+++ project.clj
@@ -7,9 +7,9 @@
   :dependencies [[org.clojure/tools.macro "0.1.2"]]
   :profiles
   {:dev
-    {:dependencies [[org.clojure/clojure "1.7.0-beta2"]
+    {:dependencies [[org.clojure/clojure "1.7.0-RC2"]
                     [org.clojure/clojurescript "0.0-3211"]
-                    [cljsjs/document-register-element "0.2.1-0"]]
+                    [cljsjs/document-register-element "0.4.3-0"]]
      :plugins [[lein-cljsbuild "1.0.5"]
                [com.cemerick/clojurescript.test "0.3.3"]]}}
   :cljsbuild
