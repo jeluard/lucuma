4d58fc235e5c4497f3a0872b6b0275c9d4efff30
Julien Eluard <julien.eluard@gmail.com>
2015-06-30 13:52:53 -0300

    Upgraded dependencies.

--- examples/project.clj
+++ examples/project.clj
@@ -1,13 +1,13 @@
 (defproject lucuma-examples "0.5.0-SNAPSHOT"
   :profiles
   {:dev
-   {:dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                   [org.clojure/clojurescript "0.0-3211"]
+   {:dependencies [[org.clojure/clojure "1.7.0"]
+                   [org.clojure/clojurescript "0.0-3308"]
                    [lucuma "0.5.0-SNAPSHOT"]
                    [prismatic/dommy "1.0.0"]
                    [hipo "0.3.0"]
-                   [cljsjs/document-register-element "0.2.1-0"]]
-    :plugins [[lein-cljsbuild "1.0.4"]
+                   [cljsjs/document-register-element "0.4.3-0"]]
+    :plugins [[lein-cljsbuild "1.0.5"]
               [com.cemerick/clojurescript.test "0.3.3"]]}}
   :cljsbuild
   {:builds
--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
   :dependencies [[org.clojure/tools.macro "0.1.2"]]
   :profiles
   {:dev
-    {:dependencies [[org.clojure/clojure "1.7.0-RC2"]
-                    [org.clojure/clojurescript "0.0-3211"]
+    {:dependencies [[org.clojure/clojure "1.7.0"]
+                    [org.clojure/clojurescript "0.0-3308"]
                     [cljsjs/document-register-element "0.4.3-0"]]
      :plugins [[lein-cljsbuild "1.0.5"]
                [com.cemerick/clojurescript.test "0.3.3"]]}}
